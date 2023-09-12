7956b9607ac6995817e763a336d4d52e44303d14
Mr Beta <144367698+server-twenty-nine@users.noreply.github.com>
2023-09-12 17:58:14 -0500

    maths
    maths

---
+++ 7_operadores_matematicos.py
@@ -0,0 +1,25 @@
+
+# Operaciones matematicas
+
+a, b = 2,3
+
+suma = a + b
+print("suma =",suma)
+
+resta = a - b
+print("resta =",resta)
+
+mul = a * b
+print("mul =",mul)
+
+div = a / b
+print("div =",div)
+
+div_ent = a // b
+print("div_ent =",div_ent)
+
+potencia = a ** b
+print("potencia =",potencia)
+
+resto = a % b
+print("resto =",resto)
